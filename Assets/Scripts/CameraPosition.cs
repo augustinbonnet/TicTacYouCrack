22904fa490c448bfcc710810b0a1e5efe6e3c55c
Augustin BONNET <augustin.bonnet@sii.fr>
2019-10-18 15:46:40 +0200

    Add base physic elements, implementing movement of the player, jump and collision with the map

---
+++ Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+
+    public Transform PlayerTransform;
+    public Vector3 PositionOffset;
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 TempVect = PlayerTransform.position + PositionOffset;
+        //TempVect.y = 3.22135f + PositionOffset.y;
+        transform.position = TempVect;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private bool isGrounded;
+    private Vector3 jump;
+    public float jumpForce = 2f;
+    public float forwardForce = 2000f;
+    public float sideWayForce = 600f;
+    public Transform RestartPoint;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Ground") && isGrounded == false)
+        {
+            Debug.Log("Test");
+            isGrounded = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (transform.position.y <= -2)
+        {
+            gameObject.transform.position = RestartPoint.position;
+            gameObject.transform.rotation = RestartPoint.rotation;
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("z"))
+        {
+            rb.AddForce(new Vector3(-forwardForce * Time.deltaTime, 0, 0),ForceMode.Force);
+        }
+        if (Input.GetKey("q"))
+        {
+            rb.AddForce(new Vector3(0, 0, -sideWayForce * Time.deltaTime));
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(new Vector3(forwardForce * Time.deltaTime, 0));
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(new Vector3(0, 0, sideWayForce * Time.deltaTime));
+        }
+        if (Input.GetKey("space") && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }        
+    }
+}
